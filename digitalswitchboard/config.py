fa776f9a8449c66f107f26ff1084fadd9ddd2fa4
Andy Gildehaus <agildehaus@gmail.com>
2013-05-24 17:15:16 -0500

    Added most of the call flow

--- digitalswitchboard/config.py
+++ digitalswitchboard/config.py
@@ -1,2 +1,4 @@
 # Twilio API credentials are stored in the system environment.
 # Set TWILIO_ACCOUNT_SID and TWILIO_AUTH_TOKEN
+
+SUNLIGHT_URL_ZIPCODE = 'http://calloncongress.sunlightfoundation.com/voice/voting/call/?language=en&zipcode=%s&next_url=https://digital-switchboard.herokuapp.com/call/zipcode-callback'
--- digitalswitchboard/views/call.py
+++ digitalswitchboard/views/call.py
@@ -1,10 +1,47 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from twilio import twiml
 
 mod = Blueprint('call', __name__)
 
-@mod.route('/', methods=['GET'])
+@mod.route('/', methods=['POST'])
 def index():
     r = twiml.Response()
-    r.say('Hello world')
+    digits = request.form.get('Digits')
+    if digits:
+        if digits == '1':
+            r.redirect('/call/zipcode')
+        elif digits == '2':
+            r.play('/static/DS5.mp3')
+            r.redirect()
+        else:
+            r.say('Sorry, I did not recognize that option.')
+    else:
+        g = r.gather(numDigits=1)
+        g.play('/static/DS1.mp3')
+        g.play('/static/DS2.mp3')
+        g.play('/static/DS3.mp3')
+        g.play('/static/DS4.mp3')
+    return str(r)
+
+@mod.route('/zipcode', methods=['POST'])
+def zipcode():
+    r = twiml.Response()
+    digits = request.form.get('Digits')
+    if digits:
+        if len(digits) < 5:
+            r.play('/static/DS7.mp3')
+            r.say(digits, voice='man')
+            r.play('/static/DS9.mp3')
+            r.redirect()
+        else:
+            r.redirect(settings.SUNLIGHT_URL_ZIPCODE % digits)
+    else:
+        g = r.gather(numDigits=5)
+        g.play('/static/DS6.mp3')
+    return str(r)
+
+@mod.route('/zipcode-callback', methods=['POST'])
+def zipcode_callback():
+    r = twiml.Response()
+    r.say('Done')
     return str(r)
